ef0f8d22972bf634fca41792788eb5ee7fef9c77
Samrudh N Arikeri <93895344+Samarikeri@users.noreply.github.com>
2025-11-19 13:23:29 +0530

    Create esp32_main.ino
    This firmware connects the ESP32 to WiFi and the MQTT broker. 
    It subscribes to the command topic and forwards all received commands to the STM32 over UART.

---
+++ esp32_firmware/esp32_main.ino
@@ -0,0 +1,44 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "YourWiFi";
+const char* password = "YourPass";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define STM32_TX 17
+#define STM32_RX 16
+
+HardwareSerial STM(2);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String cmd = "";
+  for (int i = 0; i < length; i++) cmd += (char)payload[i];
+  STM.println(cmd);
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    client.connect("ESP32_Client");
+    client.subscribe("robot/cmd");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  STM.begin(115200, SERIAL_8N1, STM32_RX, STM32_TX);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  client.setServer("broker.hivemq.com", 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected())
+    reconnect();
+
+  client.loop();
+}
